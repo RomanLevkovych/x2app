c06bb14ec6de8ef027a62c974b43b4836b3e025d
RomanLevkovych <rlevkovych098@gmail.com>
2019-03-25 14:58:31 +0200

    Initial Commit

---
+++ X2App/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  X2App
+//
+//  Created by Roman on 3/25/19.
+//  Copyright © 2019 Roman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ X2App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  X2App
+//
+//  Created by Roman on 3/25/19.
+//  Copyright © 2019 Roman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
